5bb35940abc4426f138afeff67bf0fd177109599
belzoriun <boyer.florian07@gmail.com>
2024-01-08 17:10:35 +0100

    fixed card issues

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { StatusDisplay } from './components/StatusDisplay/StatusDisplay';
 function App() {
   return (
     <div className="App">
-        <StatusDisplay sensorId="01" active="true" status="OK"></StatusDisplay>
+        <StatusDisplay sensorID="01" active="true" status="OK"></StatusDisplay>
     </div>
   );
 }
